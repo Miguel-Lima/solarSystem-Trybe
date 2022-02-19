c47292c10f0fccabc6c4c8a8b049f729f075d967
Miguel Lima <miguellimaoliver97@gmail.com>
2022-02-18 21:51:16 -0300

    SolarSystem, imports

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+    render() {
+        return (
+            <div data-testid="solar-system">
+                <p>Hello Solar System</p>
+            </div>
+        );
+    }
+}
+
+export default SolarSystem;
